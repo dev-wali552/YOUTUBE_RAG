a68b6b3d169f48b9efb9e03ab686e55f37955110
Wali <wali25560@iiitd.ac.in>
2026-06-11 17:01:58 +0530

    added frontend url

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://strong-meerkat-a392da.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
