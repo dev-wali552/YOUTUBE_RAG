69787eaaaefa3edfe1b2727ce890bda870312baf
Wali <wali25560@iiitd.ac.in>
2026-06-12 16:51:45 +0530

    error in rag.py

--- rag.py
+++ rag.py
@@ -12,12 +12,7 @@ embeddings = CohereEmbeddings(
     cohere_api_key=os.getenv("COHERE_API_KEY"),
     model="embed-english-v3.0"
 )
-vectorstore = Chroma(
-    persist_directory="./chroma_db", 
-    embedding_function=embeddings
-)
 
-retriever = vectorstore.as_retriever(search_kwargs={"k": 6})
 
 template = """You are a helpful AI Agent.Answer the user from only the youtube link the user has attached. If u cant find the desired answer then just say " I couldnt find the desired answer" .Never make up facts or number
   context: {context},
@@ -26,12 +21,19 @@ template = """You are a helpful AI Agent.Answer the user from only the youtube l
 prompt = ChatPromptTemplate.from_template(template)
 llm = ChatGroq(model="llama-3.3-70b-versatile",temperature=0)
 
-rag_chain = (
-    {"context": retriever, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
-def ask(query:str) ->str:
-    return rag_chain.invoke(query)
+
+def ask(question: str) -> str:
+    vectorstore = Chroma(
+        persist_directory="./chroma_db",
+        embedding_function=embeddings
+    )
+    retriever = vectorstore.as_retriever()
+    
+    rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return rag_chain.invoke(question)
 
